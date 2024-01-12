a4dc42ea963c97071763fffa477aeb8a0a989c91
isauritaU <dorisiulloa@gmail.com>
2024-01-12 17:31:35 -0300

    $ git add . $ git commit -am "initial commit" $ git push

--- app.py
+++ app.py
@@ -3,17 +3,35 @@ import plotly.express as px
 import streamlit as st
 
 car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
-hist_button = st.button('Construir histograma')  # crear un botón
+hist_button = st.button('Construir histograma de kilometraje')  # crear un botón
+disper_button = st.button('Relación de kilometraje y precio')
+build_hist = st.checkbox('Relación precio y año del modelo')
+build_hist_2 = st.checkbox('Modelos de vehículos')
 
+st.header('Catálogo de Vehículos US')
 
-st.header('Vehicle Catalog')
 if hist_button:  # al hacer clic en el botón
     # escribir un mensaje
     st.write(
-        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+        'Creando un histograma para el conjunto de datos de anuncios de venta de coches')
 
     # crear un histograma
-    fig = px.histogram(car_data, x="odometer")
+    fig_1 = px.histogram(car_data, x="odometer")
 
     # mostrar un gráfico Plotly interactivo
-    st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig_1, use_container_width=True)
+elif build_hist:
+    st.write('a continuación la relación entre año del modelo del vehiculo y precio')
+    fig_3 = px.histogram(car_data, x= 'price', y='model_year')
+    st.plotly_chart(fig_3, use_container_width=True)
+elif build_hist_2:
+    st.write('a continuación los modelos de los vehículos')
+    fig_4 = px.histogram(car_data, x= 'model')
+    st.plotly_chart(fig_4, use_container_width=True)
+else disper_button:
+    st.write('creando un gráfico de dispersión en relación kilometraje/precio')
+    fig_2 = px.scatter(car_data, x="odometer", y="price")
+    st.plotly_chart(fig_2, use_container_width=True)
+
+
+
