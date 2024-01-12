e0e8152eafc23dfa380fcbb1bb14129c166d8f21
isauritaU <dorisiulloa@gmail.com>
2024-01-12 17:39:49 -0300

    $ git add . $ git commit -am "initial commit" $ git push

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import plotly.express as px
 import streamlit as st
 
 car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
-hist_button = st.button('Construir histograma de kilometraje')  # crear un botón
+hist_button = st.button(
+    'Construir histograma de kilometraje')  # crear un botón
 disper_button = st.button('Relación de kilometraje y precio')
 build_hist = st.checkbox('Relación precio y año del modelo')
 build_hist_2 = st.checkbox('Modelos de vehículos')
@@ -22,16 +23,13 @@ if hist_button:  # al hacer clic en el botón
     st.plotly_chart(fig_1, use_container_width=True)
 elif build_hist:
     st.write('a continuación la relación entre año del modelo del vehiculo y precio')
-    fig_3 = px.histogram(car_data, x= 'price', y='model_year')
+    fig_3 = px.histogram(car_data, x='price', y='model_year')
     st.plotly_chart(fig_3, use_container_width=True)
 elif build_hist_2:
     st.write('a continuación los modelos de los vehículos')
-    fig_4 = px.histogram(car_data, x= 'model')
+    fig_4 = px.histogram(car_data, x='model')
     st.plotly_chart(fig_4, use_container_width=True)
-else disper_button:
+elif disper_button:
     st.write('creando un gráfico de dispersión en relación kilometraje/precio')
     fig_2 = px.scatter(car_data, x="odometer", y="price")
     st.plotly_chart(fig_2, use_container_width=True)
-
-
-
